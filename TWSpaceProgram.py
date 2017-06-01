334131219e8fdf2ee2782146c5dd5fbef6393aa3
Justin Focus <iamjustinfocus@gmail.com>
2017-06-02 00:35:46 +0100

    TODO

--- TWSpaceProgram.py
+++ TWSpaceProgram.py
@@ -1,14 +1,17 @@
 '''TW Space Program, launching Rovers onto mars!'''
 
-import os # Required to read text files
-import sys # Required to read command line args
+import os 
+import sys 
 
 from shipyard import Rover
+from space import Planet
+
 
 def upload_data(testfile):
     '''Opens and processes test file when called by launch()'''
     query = os.path.isfile(testfile)
     if query == False:
+        #TODO: Make this check more robust
         raise ValueError
     data = open(testfile, "r").readlines() 
     data = [line.strip('\n').split(' ') for line in data]
@@ -21,8 +24,9 @@ def launch(testfile):
     except ValueError:
         print('Failed launch, Bad Data. Please format text file correctly.')
         return 1
-    planet = data.pop(0)
+    target = data.pop(0)
+    planet = Planet(lat=int(target[0]), lon=int(target[1]))
     rovers = []
     for i in range(int(len(data) / 2)):
-        rovers.append(Rover(landing=data[i * 2], commands=data[(i * 2) + 1]))
+        rovers.append(Rover(landing=data[i * 2], commands=data[(i * 2) + 1], planet=planet))
     return rovers
