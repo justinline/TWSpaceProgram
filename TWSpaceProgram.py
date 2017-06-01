c7efeee40ed6c0008dbb604fcdb6cb897457fe09
Justin Focus <iamjustinfocus@gmail.com>
2017-06-02 00:06:50 +0100

    split Rover() into its own app

--- TWSpaceProgram.py
+++ TWSpaceProgram.py
@@ -3,6 +3,7 @@
 import os # Required to read text files
 import sys # Required to read command line args
 
+from shipyard import Rover
 
 def upload_data(testfile):
     '''Opens and processes test file when called by launch()'''
@@ -12,7 +13,7 @@ def upload_data(testfile):
     data = open(testfile, "r").readlines() 
     data = [line.strip('\n').split(' ') for line in data]
     return data
-    
+
 def launch(testfile):
     '''Function to start the mission! Initialises Rovers with instructions'''
     try:
@@ -25,30 +26,3 @@ def launch(testfile):
     for i in range(int(len(data) / 2)):
         rovers.append(Rover(landing=data[i * 2], commands=data[(i * 2) + 1]))
     return rovers
-
-class Rover(object):
-    '''Rover object that follows commands'''
-    compass = {'N':0,'E':1,'S':2,'W':3}
-    turn = {'L':-1, 'R':1}
-    rules =  [(0,1), (1,0), (0,-1), (-1,0)]
-    def __init__(self, landing, commands):
-        self.x = int(landing[0])
-        self.y = int(landing[1])
-        self.heading = self.compass[landing[2]]
-        self.commands = list(commands[0]) # Breaks string into list of chars
-
-    def path(self):
-        for i in self.commands:
-            print(i)
-    
-    def rotate(self, direction):
-        direction = self.turn[direction]
-        self.heading = (self.heading + direction) % 4
-
-    def move(self):
-        self.x += self.rules[self.heading][0]
-        self.y += self.rules[self.heading][1]
-    def report(self):
-        print(self.x, self.y, self.heading)
-
-    pass
---
+++ shipyard.py
@@ -0,0 +1,28 @@
+class Rover(object):
+    '''Rover object that follows commands'''
+    # On-board Navigation Mapping 'Bzzt
+    compass = {'N':0,'E':1,'S':2,'W':3}
+    turn = {'L':-1, 'R':1}
+    rules =  [(0,1), (1,0), (0,-1), (-1,0)]
+    
+    def __init__(self, landing, commands):
+        self.x = int(landing[0])
+        self.y = int(landing[1])
+        self.heading = self.compass[landing[2]]
+        self.commands = list(commands[0])
+
+    def path(self):
+        for i in self.commands:
+            print(i)
+    
+    def rotate(self, direction):
+        direction = self.turn[direction]
+        self.heading = (self.heading + direction) % 4
+
+    def move(self):
+        self.x += self.rules[self.heading][0]
+        self.y += self.rules[self.heading][1]
+
+    def report(self):
+        print(self.x, self.y, self.heading)
+
