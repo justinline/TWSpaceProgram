ad221dc64dd14c6cc418617fc767056f6509c9a3
Justin Focus <iamjustinfocus@gmail.com>
2017-06-02 00:33:11 +0100

    Cleaned up

--- space.py
+++ space.py
@@ -1,5 +1,4 @@
 class Planet(object):
+    '''A planet object that specifies a n*n rectangular terrain'''
     def __init__(self, lat=0, lon=0):
-        self.lat = lat
-        self.lon = lon
-        self.area = (self.lat, self.lon)
+        self.area = (lat, lon)
