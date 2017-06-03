447943d535babc868388a5d8cc793a559428c10d
Justin Focus <iamjustinfocus@gmail.com>
2017-06-03 17:03:10 +0100

    linted and refactored

--- tw_space_program.py
+++ tw_space_program.py
@@ -1,6 +1,6 @@
 '''TW Space Program, launching Rovers onto mars!'''
 
-import sys 
+import sys # argv, exit
 
 from shipyard import Rover
 from space import Planet
@@ -9,23 +9,26 @@ class MissionData(object):
     '''Object for Storing Mission Test Files'''
     def __init__(self, filepath):
         self.lines = open(filepath, "r").readlines()
+        self.planet = []
+        self.launch_cmds = []
 
 def clean(line, split=False):
+    '''Cleans up text file input'''
     line = line.strip('\n')
     if split:
         line = line.split(' ')
     return line
 
 def parse_test_data(filepath):
-    '''Opens and processes test file into a list of lists when called by launch()'''
+    '''Opens and processes test file into MissionData Obj'''
     try:
-        data = MissionData(filepath)    
+        data = MissionData(filepath)
     except IOError:
         print("Launch Test Data was not found at the specified location:", filepath)
         return sys.exit(1)
     data.planet = clean(data.lines[0], split=True)
     f = data.lines[1:]
-    data.launch_cmds = [(clean(f[i], split=True),clean(f[i+1])) for i in range(0,len(f),2)]
+    data.launch_cmds = [(clean(f[i], split=True), clean(f[i+1])) for i in range(0, len(f), 2)]
     return data
 
 def initialise_rovers(filepath):
@@ -36,8 +39,9 @@ def initialise_rovers(filepath):
     return rovers
 
 def main(args):
+    '''Run command line arguments'''
     if len(args) == 2:
-        rovers = initialise_rovers(sys.argv[1])
+        rovers = initialise_rovers(args[1])
         for rover in rovers:
             rover.execute_commands()
             print(rover.report())
