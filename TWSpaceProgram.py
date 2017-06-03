0a2845afdb1a61986d1f1c5e57fb5bb4e0937805
Justin Focus <iamjustinfocus@gmail.com>
2017-06-03 14:35:03 +0100

    added MissionData obj and added cleaner data parsing

--- TWSpaceProgram.py
+++ TWSpaceProgram.py
@@ -5,26 +5,30 @@ import sys
 from shipyard import Rover
 from space import Planet
 
+class MissionData(object):
+    '''Object for Storing Mission Test Files'''
+    def __init__(self, filepath):
+        self.file = open(filepath, "r").readlines()
+    pass
 
-def upload_data(filepath):
+def parse_test_data(filepath):
     '''Opens and processes test file into a list of lists when called by launch()'''
     try:
-        data = open(filepath, "r").readlines() 
+        data = MissionData(filepath)    
     except IOError:
         print("Launch Test Data was not found at the specified location:", filepath)
         print("Please check file exists and try again")
         return sys.exit(1)
-    data = [line.strip('\n').split(' ') for line in data]
+    data.planet = data.file[0].strip('\n').split(' ')
+    f = data.file[1:]
+    data.rovers = [(f[i].strip('\n').split(' '),f[i+1].strip('\n')) for i in range(0,len(f),2)]
     return data
 
 def launch(filepath):
     '''Function to start the mission! Initialises Rovers with instructions'''
-    data = upload_data(filepath)
-    target = data.pop(0)
-    planet = Planet(lat=int(target[0]), lon=int(target[1]))
-    rovers = []
-    for i in range(int(len(data) / 2)):
-        rovers.append(Rover(landing=data[i * 2], commands=data[(i * 2) + 1], planet=planet))
+    data = parse_test_data(filepath)
+    planet = Planet(lat=int(data.planet[0]), lon=int(data.planet[1]))
+    rovers = [Rover(landing=cmd[0], commands=cmd[1], planet=planet) for cmd in data.rovers]
     return rovers
 
 if len(sys.argv) > 1:
